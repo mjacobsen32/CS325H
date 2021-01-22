3f7cc6aa97be72a9c13cb06feee879aaaa710ad6
Ethan Hampton <ethohampton@gmail.com>
2021-01-21 16:40:46 -0800

    Test improvements

--- assignment1/test_assignment1.py
+++ assignment1/test_assignment1.py
@@ -8,16 +8,16 @@ class CrossingsTest(TestCase):
         output_file_path = "output/output1.txt"
         count_crossings("test_input/sample1.txt", output_file_path)
         with open(output_file_path, "r") as output_file:
-            self.assertEqual(output_file.readline(), "3")
+            self.assertEqual("3", output_file.readline(), "Sample 1")
         count_crossings("test_input/sample2.txt", output_file_path)
         with open(output_file_path, "r") as output_file:
-            self.assertEqual(output_file.readline(), "3")
+            self.assertEqual("3", output_file.readline(), "Sample 2")
         count_crossings("test_input/sample2.txt", output_file_path)
         with open(output_file_path, "r") as output_file:
-            self.assertEqual(output_file.readline(), "1")
+            self.assertEqual("1", output_file.readline(), "Sample 3")
 
 
 class IOTest(TestCase):
     def test_array_input(self):
-        self.assertEqual(array_input("test_input/sample1.txt", 1), ["1", "2", "3"])
-        self.assertEqual(array_input("test_input/sample1.txt", 2), ["3", "2", "1"])
+        self.assertEqual(["1", "2", "3"], array_input("test_input/sample1.txt", 1), "Line 1")
+        self.assertEqual(["3", "2", "1"], array_input("test_input/sample1.txt", 2), "Line 2")
