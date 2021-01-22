21e1ad1c450e933f10082d093ead036b9546369b
Ethan Hampton <ethohampton@gmail.com>
2021-01-21 16:05:32 -0800

    Add simple tests

---
+++ assignment1/test_assignment1.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from assignment1 import count_crossings
+
+
+class Test(TestCase):
+    def test_count_crossings(self):
+        output_file_path = "output/output1.txt"
+        count_crossings("test_input/sample1.txt", output_file_path)
+        with open(output_file_path, "r") as output_file:
+            self.assertEqual(output_file.readline(), "3")
+        count_crossings("test_input/sample2.txt", output_file_path)
+        with open(output_file_path, "r") as output_file:
+            self.assertEqual(output_file.readline(), "3")
+        count_crossings("test_input/sample2.txt", output_file_path)
+        with open(output_file_path, "r") as output_file:
+            self.assertEqual(output_file.readline(), "1")
